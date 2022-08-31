bec1ab1b0dd9c30136bdbe5c767003149cfd82ff
Jeferson-Labout <jefson1989@gmail.com>
2022-08-31 00:00:11 -0300

    create chamado

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './auth/auth.guard';
+import { ChamadoCreateComponent } from './components/chamado/chamado-create/chamado-create.component';
 import { ChamadoListComponent } from './components/chamado/chamado-list/chamado-list.component';
 import { ClienteCreateComponent } from './components/cliente/cliente-create/cliente-create.component';
 import { ClienteListComponent } from './components/cliente/cliente-list/cliente-list.component';
@@ -27,7 +28,8 @@ const routes: Routes = [
       { path: 'clientes/delete/:id', component: ClienteCreateComponent },
 
 
-      {path: 'chamados' , component: ChamadoListComponent }
+      { path: 'chamados', component: ChamadoListComponent },
+      { path: 'chamados/create', component: ChamadoCreateComponent }
     ]
   }
 ];
