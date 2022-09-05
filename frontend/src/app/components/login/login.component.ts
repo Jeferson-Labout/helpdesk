4c948c13a5313062b6e329abdaf742d58ef40dc6
Jeferson Labout <70985458+Jeferson-Labout@users.noreply.github.com>
2022-09-05 10:46:39 -0300

    finalizando projeto

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { Credenciais } from 'src/app/models/credenciais';
@@ -11,25 +11,32 @@ import { AuthService } from 'src/app/services/auth.service';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  creds: Credenciais ={
-    email: '', 
-    senha: ''
-  }
 
-  email = new FormControl(null, Validators.email);
-  senha = new FormControl(null, Validators.minLength(3));
+
+  loginForm!: FormGroup;
+
 
   constructor( private toast: ToastrService,
-    private service: AuthService, private router: Router) { }
+    private service: AuthService, private router: Router,
+    private formBuilder: FormBuilder,) { }
 
   ngOnInit(): void {
+    this.criarFormulario();
   }
  validaCampos(): boolean {
-    return this.email.valid && this.senha.valid
+    return this.loginForm.get('email').valid && this.loginForm.get('senha').valid;
   }
   
+  criarFormulario() {
+    
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      senha: ['', [Validators.required]],
+    });
+  }
+
   logar() {
-    this.service.authenticate(this.creds).subscribe(resposta => {
+    this.service.authenticate(this.loginForm.value).subscribe(resposta => {
       this.service.successfulLogin(resposta.headers.get('Authorization').substring(7));
       this.router.navigate([''])
     }, () => {
